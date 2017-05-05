fea9187d8d3f534d902cb24ab5f6cba9c451288b
Jeff Charlesworth <jcharlesworth02@gmail.com>
2017-05-05 13:08:41 -0700

    store auth token, request user information

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,18 +1,19 @@
 #require "kele/version.rb"
 require 'httparty'
 
-module Kele
+class Kele
   include HTTParty
-  base_uri =  'www.bloc.io/api/v1'
+  @base_uri =  'https://www.bloc.io/api/v1'
 
   def self.new(e, p)
-    response = self.post('https://www.bloc.io/api/v1/sessions/', query: { email: "#{e}", password: "#{p}" } )
+    response = self.post(@base_uri + '/sessions/', query: { email: "#{e}", password: "#{p}" } )
     @auth_token = response["auth_token"]
-    puts @auth_token
+    puts response.body 
+    # cookies('authorization' => @auth_token)
   end
 
   def self.user
-    response = get('https://www.bloc.io/api/v1/users/me')
+    response = get('https://www.bloc.io/api/v1/users/me', headers: { 'authorization' => @auth_token })
     puts response
   end
 end
