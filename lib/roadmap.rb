64b4d17f99cb88a6c5258455ac7a67092fcb355c
Jeff Charlesworth <jcharlesworth02@gmail.com>
2017-05-11 14:33:39 -0700

    test commit, add create_submission

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -6,11 +6,24 @@ module Roadmap
 
   def self.get_roadmap(id = nil)
     roadmap = get($base + "/roadmaps/#{id || $roadmap_id}", $auth_token)
-    JSON.parse!(roadmap.body)
+#    JSON.parse!(roadmap.body)
+    JSON.pretty_generate(roadmap)
   end
 
   def self.get_checkpoint(id)
     checkpoint = get($base + "/checkpoints/#{id}", $auth_token)
     JSON.parse!(checkpoint.body)
+  end
+
+  def self.create_submission(assignment_branch, assignment_commit_link, checkpoint_id, comment, enrollment_id)
+    options = $auth_token
+    options[:query] = {
+      assignment_branch: assignment_branch,
+      assignment_commit_link: assignment_commit_link,
+      checkpoint_id: checkpoint_id,
+      comment: comment,
+      enrollment_id: enrollment_id
+    }
+    submission = post($base + "/checkpoint_submissions", options)
   end
 end
