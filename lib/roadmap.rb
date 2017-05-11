66ed3a59a79a18c992bb6dc8d1dea23a3ea477e1
Jeff Charlesworth <jcharlesworth02@gmail.com>
2017-05-11 15:08:35 -0700

    extra credit

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -6,8 +6,7 @@ module Roadmap
 
   def self.get_roadmap(id = nil)
     roadmap = get($base + "/roadmaps/#{id || $roadmap_id}", $auth_token)
-#    JSON.parse!(roadmap.body)
-    JSON.pretty_generate(roadmap)
+    JSON.parse!(roadmap.body)
   end
 
   def self.get_checkpoint(id)
@@ -25,5 +24,17 @@ module Roadmap
       enrollment_id: enrollment_id
     }
     submission = post($base + "/checkpoint_submissions", options)
+  end
+
+  def self.update_submission(id, assignment_branch, assignment_commit_link, checkpoint_id, comment, enrollment_id)
+    options = $auth_token
+    options[:query] = {
+      assignment_branch: assignment_branch,
+      assignment_commit_link: assignment_commit_link,
+      checkpoint_id: checkpoint_id,
+      comment: comment,
+      enrollment_id: enrollment_id
+    }
+    submission = post($base + "/checkpoint_submissions/#{id}", options)
   end
 end
